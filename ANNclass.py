e805cfa72972335adfdc117691911d5b20957b53
Vinícius Lima <vinicius.lima.cordeiro@usp.br>
2018-11-14 12:30:11 +0100

    Update ANNclass.py
    ALLOWS MINI-BATCH.

--- ANNclass.py
+++ ANNclass.py
@@ -1,72 +1,136 @@
-# Artificial Neural Network
+import numpy as np 
+import matplotlib.pyplot as plt 
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+class ANN:
 
-# Installing Tensorflow
-# pip install tensorflow
+	def __init__(self, layers = (1,), X=None, y=None, epochs=10000, batch_size = 10, alpha=1, seed=1, verbose=True):
+		'''
+			Constructor method. Build the ANN
+			Inputs:
+			layer: Tuple with number of neurons in each hidden layer
+			input: Input values to the ANN
+			output: Output used to train the weights
+			alpha: Learning rate
+			seed:  Random seed to be used
+			epochs: Number of epochs
+		'''
+		# Set seed
+		np.random.seed(seed)
+		self.verbose = verbose
+		self.batch_size = batch_size
+		self.layers = layers                   # How many neurons in each hidden layer, the last one is the output layer (tuple)
+		self.epochs = epochs                   # Number of epochs (iteractions)
+		self.alpha  = alpha                    # Learning rate
+		self.X      = X                        # Input values
+		self.y      = y                        # Output values (labels)
+		self.n_layers    = len(self.layers)        # Number of hidden layers
+		self.n_input_layers   = self.X.shape[1]    # Number of neurons in the input layer
+		self.m                = float(self.X.shape[0]) # Number of training sets
+		self.a                = {}                 # Dictionary to store neurons activation in each layer
+		for i in range(self.n_layers+1):           # Initializing one entrie for each hidden layer
+			if i == 0:
+				self.a[i] = self.X
+			else:
+				self.a[i] = []
+		# Creating random weights
+		self.weights = {}                          # Weights dictionary
+		self.bias    = {}                          # Bias dictionary
+		# Initializing weights for the input layer
+		self.weights[0] = np.random.normal( 0, 1, size=(self.n_input_layers, self.layers[0]) )
+		# Initializing weights for the first hidden layer
+		self.bias[0] = np.random.normal( 0, 1, size=(self.layers[0], 1) )
+		# If there is more then one hidden layer, initialize the weights between then
+		if self.n_layers > 1:
+			for i in range(self.n_layers - 1):
+				self.weights[i+1] = np.random.normal( 0, 1, size=(self.layers[i], self.layers[i+1]) )
+				self.bias[i+1]    = np.random.normal( 0, 1, size=(self.layers[i+1], 1) )
 
-# Installing Keras
-# pip install --upgrade keras
+	def fit(self,):
+		'''	
+			Fit the weights in the neural net.
+		'''
+		self.ypred = []
+		for i in range(self.epochs):
+			if self.verbose == True:
+				print 'epoch = ' + str(i) + '...'
+			for k in range(0, int(self.m), self.batch_size):
+				self.a[0]  = self.X[k:k+10, :].copy()
+				self.yexpc = self.y[k:k+10, :].copy()
+				for j in range(self.n_layers):
+					self.a[j+1] = self.sigmoid( np.dot( self.a[j], self.weights[j] ) + self.bias[j].T )  # Compute input in the jth in the hidden layer
+				# Backpropagation algorithm
+				# For mathematical details see:
+				# http://neuralnetworksanddeeplearning.com/chap2.html
+				self.delta = self.cost_function_prime(self.yexpc, self.a[self.n_layers]) * self.sigmoid_prime(self.a[self.n_layers])            # Compute error [delta = dC/da]
+				self.weights[self.n_layers-1] -= (self.alpha/self.m) * np.dot(self.a[self.n_layers-1].T, self.delta)  # Update weights
+				self.bias[self.n_layers-1] -= self.alpha * self.delta.mean()
+				for L in range(2, self.n_layers+1):
+					self.delta = np.dot( self.delta, self.weights[self.n_layers-(L-1)].T ) *  self.sigmoid_prime(self.a[self.n_layers-(L-1)])
+					self.weights[self.n_layers-L] -= (self.alpha/self.m) * np.dot(self.a[self.n_layers-L].T, self.delta)  # Update weights
+					self.bias[self.n_layers-L] -= self.alpha * self.delta.mean()
 
-# Part 1 - Data Preprocessing
 
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from ANNclass import ANN
+	def predict(self, Xpred):
+		'''
+			Predict the output of a given set Xpred. Must be called after network fitting.
+			Inputs:
+			Xpred: Data set which will be used to predict an output.
+			Outputs:
+			ypred: ANN predction output 
+		'''
+		self.X = Xpred                      # Predict values
+		for i in range(self.n_layers+1):    # Re-initializing activation vector for each hidden layer
+			if i == 0:
+				self.a[i] = self.X
+			else:
+				self.a[i] = []
+		# Forward pass Xpred. Since we've already fitted the weights we don't need the backpropagation
+		# for weight corretion, we just forward pass once.
+		for j in range(self.n_layers):
+				self.a[j+1] = self.sigmoid( np.dot( self.a[j], self.weights[j] ) + self.bias[j].T )
+		return self.a[self.n_layers]
 
-def score(y, y_pred):
-	'''
-		Scores accuracy of ANN.
-		Inputs:
-		y: Real value to be predict
-		y_pred: Predictions made be the ANN.
-		Outputs:
-		score: Pontuation score (%) of the ANN.
-	'''
-	score = 0
-	for i in range(len(y)):
-		if y[i] == y_pred[i]:
-			score += 1
-	return float(score) / len(y) * 100
+	def sigmoid(self, x):
+		'''
+			Sigmoid function, computes the output of f(x), where x in the input
+			and f is the sigmoid function f(x) = 1 / (1 + exp(-x)).
+			Inputs:
+			x: Input value
+			Outputs:
+			Return f(x)
+		'''
+		return 1 / (1 + np.exp(-x) )
 
-# Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
+	def sigmoid_prime(self, output):
+		'''
+			The first derivative of the sigmoid function, computes the output of f(x), where x in the input
+			and f is the sigmoid function f'(x) = f(x)*(1-f(x)).
+			Inputs:
+			x: Input value
+			Outputs:
+			Return f'(x)
+		'''
+		return output * ( 1 - output )
 
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
-X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:]
+	def cost_function(self, y, yprime):
+		'''
+			Return the quadratic cost function  C = 1/2 * (y-yprime)^2, between the expected value and estimated value.
+			Inputs:
+			y: Expected value
+			yprime: Estimated value
+			Outputs:
+			Return C
+		'''
+		return 0.5 * (y - yprime)**2
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-# Reshaping y array
-y_train = y_train.reshape((X_train.shape[0],1))
-y_test  = y_test.reshape((X_test.shape[0],1))
-
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-ann = ANN(layers=(6,6,1), X=X_train, y=y_train, batch_size = 10, epochs=100, alpha=20, seed=546)
-ann.fit()
-y_pred = ann.predict(X_test)
-# Binarizing y_pred
-y_pred = (y_pred>0.5).astype(int)
-
-acc = score(y_test, y_pred)
-print 'ANN accuracy is ' + str(acc) + '%.'
-
-#ann.predict(sc.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
+	def cost_function_prime(self, y, yprime):
+		'''
+			Return the first derivative of the quadratic cost function  C' = (y-yprime), 
+			between the expected value and estimated value.
+			Inputs:
+			y: Expected value
+			yprime: Estimated value
+			Outputs:
+			Return C'
+		'''
+		return (yprime - y)
